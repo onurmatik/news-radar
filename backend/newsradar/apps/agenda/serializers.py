7e2f30ac394b6e1807154ff87039c0731d4fab3b
Kemal Soylu <kemalbsoylu@gmail.com>
2026-01-06 17:49:43 +0300

    removed restapi

--- backend/newsradar/apps/agenda/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class SimilarContentRequestSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=500)
-    date = serializers.DateField()
-    threshold = serializers.FloatField(default=0.8, min_value=0.0, max_value=1.0)
-    limit = serializers.IntegerField(default=10, min_value=1, max_value=100)
--- backend/newsradar/apps/agenda/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import SimilarContentView
-
-urlpatterns = [
-    path("similar-content/", SimilarContentView.as_view(), name="similar-content"),
-]
