31c0559c83703dbc9c364efa586f85456a925de0
Kemal Soylu <kemalbsoylu@gmail.com>
2026-01-06 16:36:47 +0300

    added admins

--- backend/newsradar/apps/keywords/admin.py
+++ backend/newsradar/apps/keywords/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Board, Keyword, KeywordGroup, RssSource
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "owner", "created_at")
+    list_select_related = ("owner",)
+    search_fields = ("name", "owner__email", "owner__username")
+
+
+@admin.register(KeywordGroup)
+class KeywordGroupAdmin(admin.ModelAdmin):
+    list_display = ("id", "board", "name")
+    list_select_related = ("board",)
+    search_fields = ("name",)
+
+
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ("id", "board", "text", "status", "search_interval_minutes")
+    list_select_related = ("board", "group")
+    list_filter = ("status",)
+    search_fields = ("text",)
+
+
+@admin.register(RssSource)
+class RssSourceAdmin(admin.ModelAdmin):
+    list_display = ("id", "board", "name", "feed_url", "enabled")
+    list_select_related = ("board",)
+    search_fields = ("name", "feed_url")
