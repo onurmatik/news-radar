e8f3c4c86645b83a92b5d98c35f8b01676b7ba06
Kemal Soylu <kemalbsoylu@gmail.com>
2026-01-14 17:30:13 +0300

    added user models

--- newsradar/accounts/models.py
+++ newsradar/accounts/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.first_name.strip() or self.user.username
+
+
+class UserKeyword(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='keywords')
+    keyword = models.ForeignKey('keywords.Keyword', on_delete=models.CASCADE, related_name='keywords_by')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'keyword'], name='unique_user_keyword')
+        ]
