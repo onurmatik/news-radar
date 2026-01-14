e7c826c0c9a708cee5f1868df15860cb3541d44d
Kemal Soylu <kemalbsoylu@gmail.com>
2026-01-14 18:45:09 +0300

    admin updates

--- newsradar/accounts/admin.py
+++ newsradar/accounts/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newsradar.accounts.models import Profile, UserKeyword
+
+User = get_user_model()
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    extra = 0
+
+
+class UserKeywordInline(admin.TabularInline):
+    model = UserKeyword
+    extra = 0
+    autocomplete_fields = ("keyword",)
+
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
+
+@admin.register(User)
+class AccountUserAdmin(UserAdmin):
+    inlines = (ProfileInline, UserKeywordInline)
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+    list_filter = ("is_staff", "is_active", "date_joined")
+    search_fields = ("username", "email", "first_name", "last_name")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+    search_fields = ("user__username", "user__email")
+    list_select_related = ("user",)
+
+
+@admin.register(UserKeyword)
+class UserKeywordAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "keyword")
+    search_fields = ("user__username", "user__email", "keyword__text")
+    list_select_related = ("user", "keyword")
--- newsradar/keywords/admin.py
+++ newsradar/keywords/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from newsradar.accounts.models import UserKeyword
 from .models import Keyword
 from newsradar.executions.tasks import web_search_execution
 
@@ -10,6 +11,11 @@ class KeywordAdmin(admin.ModelAdmin):
     list_filter = ("provider", "last_fetched_at", "created_at")
     search_fields = ("text", "query")
     actions = ("run_web_search",)
+
+    def save_model(self, request, obj, form, change) -> None:
+        super().save_model(request, obj, form, change)
+        if not change and request.user.is_authenticated:
+            UserKeyword.objects.get_or_create(user=request.user, keyword=obj)
 
     @admin.action(description="Run web search execution for selected keywords")
     def run_web_search(self, request, queryset):
